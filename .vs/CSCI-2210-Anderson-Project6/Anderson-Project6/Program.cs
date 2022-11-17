d2c3c8bfcea7dd785253d375619674bc39419133
Andersonjb1 <Blainea544@gmail.com>
2022-11-16 21:17:57 -0500

    Tree Set Up

--- .vs/CSCI-2210-Anderson-Project6/Anderson-Project6/Program.cs
+++ .vs/CSCI-2210-Anderson-Project6/Anderson-Project6/Program.cs
@@ -4,28 +4,14 @@
     {
         static void Main(string[] args)
         {
-            String filepath = "C:\\Users\\BLAIN\\OneDrive - East Tennessee State University\\Classes\\Data Structures\\Project 6\\books.csv";
+            AVLTree tree = new AVLTree();
 
-            StreamReader reader = new StreamReader(filepath);
-            List<string> lineList = new();
-            List<string> wordList = new();
-            Book book = new();
-
-            while(!reader.EndOfStream)
-            {
-                lineList.Add(reader.ReadLine());
-            }
-            for (int i = 0; i < lineList.Count; i++)
-            {
-                book.Title = lineList[i].Split(",");
-                lineList[i + 1].Split(",");
-                lineList[i + 2].Split(",");
-                lineList[i + 3].Split(",");
-                lineList[i + 4].Split(",");  
-                Console.WriteLine(wordList[35]);
-            }
-            //Book book = new Book(reader.ReadLine);
-            //book.Print();
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(7);
+            tree.Add(2);
+            tree.Delete(7);
+            tree.DisplayTree();
         }
     }
 }
