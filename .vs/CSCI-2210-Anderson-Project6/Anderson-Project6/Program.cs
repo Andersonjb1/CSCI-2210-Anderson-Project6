dfe8d1825ddb72bf8d30f7420a85206645080c43
Andersonjb1 <Blainea544@gmail.com>
2022-11-16 21:44:17 -0500

    Books inserting into the tree, need to work on inserting the file now.

--- .vs/CSCI-2210-Anderson-Project6/Anderson-Project6/Program.cs
+++ .vs/CSCI-2210-Anderson-Project6/Anderson-Project6/Program.cs
@@ -5,13 +5,19 @@
         static void Main(string[] args)
         {
             AVLTree tree = new AVLTree();
-
-            tree.Add(5);
-            tree.Add(3);
-            tree.Add(7);
-            tree.Add(2);
-            tree.Delete(7);
+            Book book = new Book("aba","bab", 32, "aab");
+            Book bookie= new Book("tit", "vag", 69, "po");
+            Book zzzzz = new Book("z", "a", 1000, "ooo");
+            Book test = new Book(" ", "dd", 0, "");
+            Book test2 = new Book(" ", "22", 1, "");
+            tree.Add(zzzzz);
+            tree.Add(book);
+            tree.Add(bookie);
+            tree.Find(zzzzz);
+            tree.Delete(zzzzz);
+            tree.Delete(book);
             tree.DisplayTree();
+            //Git is not working properly
         }
     }
 }
