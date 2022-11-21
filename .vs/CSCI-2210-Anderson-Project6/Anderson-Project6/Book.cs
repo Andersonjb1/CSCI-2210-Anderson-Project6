1f1c23c167a300e926c1dc52d71fa2f200d8d546
Andersonjb1 <Blainea544@gmail.com>
2022-11-21 15:10:55 -0500

    Commenting halfway done

--- .vs/CSCI-2210-Anderson-Project6/Anderson-Project6/Book.cs
+++ .vs/CSCI-2210-Anderson-Project6/Anderson-Project6/Book.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿///////////////////////////////////////////////////////////////////////////////
+//
+// Author: Blaine Anderson, Andersonjb1@etsu.edu
+// Course: CSCI-2210-001 - Data Structures
+// Assignment: Project 6
+// Description: Project demonstrating understanding of AVLTrees
+//
+///////////////////////////////////////////////////////////////////////////////
+using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Runtime.CompilerServices;
@@ -6,19 +14,47 @@ using System.Text;
 
 namespace Anderson_Project6
 {
+    /// <summary>
+    /// 
+    /// </summary>
     internal class Book : IComparable<Book>
     {
-        public string? Key;
-        public string? Title { get; set; }
-        public string? Author { get; set; }
-        public int? Pages { get; set; }
-        public string? Publisher { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Key { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Author { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Pages { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Publisher { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public Book()
         {
 
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="author"></param>
+        /// <param name="pages"></param>
+        /// <param name="publisher"></param>
         public Book(string title, string author, int pages, string publisher)
         {
             this.Title = title;
@@ -27,6 +63,9 @@ namespace Anderson_Project6
             this.Publisher = publisher;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public void Print() //displays the title, author, number of pages, and publisher of the book
         {
             Console.ForegroundColor = ConsoleColor.Red;
@@ -38,6 +77,11 @@ namespace Anderson_Project6
 
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="book"></param>
+        /// <returns></returns>
         public int CompareTo(Book? book)
         {
             if (Key.CompareTo(book.Key) == 0)
@@ -45,23 +89,26 @@ namespace Anderson_Project6
                 return 0;
             }
             else if (Key.CompareTo(book.Key) == -1)
-            { 
+            {
                 return -1;
             }
             else return 1;
 
-        }
+        }//comment to let me commit to GitHub
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
         public void SetKey(string input)
         {
             Dictionary<string, Action> keySetter = new();
             keySetter["TITLE"] = () => { Key = this.Title; };
             keySetter["AUTHOR"] = () => { Key = this.Author; };
             keySetter["PUBLISHER"] = () => { Key = this.Publisher; };
+
             if (keySetter.ContainsKey(input.ToUpper()))
                 keySetter[input.ToUpper()]();
-            else
-                SetKey(input);
         }
     }
 }
